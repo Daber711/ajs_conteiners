6a04acec5c7fdc352d66449c8bfd500236e9e93e
Alexey Sokolov <dabero7111@gmail.com>
2024-04-25 19:36:03 +0300

    conteiners

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,28 @@
+import Team from "../app.js";
+import ErrorRepository from "../app.js";
+import Character from "../class/Character.js";
+
+test("Test Team", () => {
+  const hero1 = new Character("Ivan", "Bowman");
+  const team = new Team();
+  team.add(hero1);
+  const result = 
+  {
+    name: "Ivan",
+    type: "Bowman",
+    health: 100,
+    level: 1,
+    attack: undefined,
+    defence: undefined,
+  };
+
+  expect(team[0]).toEqual(result);
+});
+
+test("Test ErrRep", ()=> {
+  const errRep = new ErrorRepository();
+  errRep.addErr(123, 'Ошибка 123');
+  const result = errRep.translate(123);
+
+  expect(123).toEqual(result);
+});
---
+++ src/js/app.js
@@ -0,0 +1,49 @@
+import Character from "../js/class/Character.js";
+
+export class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(obj) {
+    if (this.members.has(obj)) {
+      throw new Error('Пресонаж уже добавлен');
+    } else this.members.add(obj);
+  }
+
+  addAll(...obj) {
+    obj.forEach((elem) => {
+      if (this.members.has(elem)) {
+        throw new Error('Пресонаж уже добавлен');
+      }
+      else this.members.add(elem);
+    });
+  }
+
+  toArray() {
+    const arrTeam = Array.from(this.members);
+    return arrTeam;
+  }
+}
+
+
+export class ErrorRepository {
+  constructor() {
+    this.errBase = new Map();
+  }
+
+  addErr(code, message) {
+    this.errBase.set({code, message});
+  }
+
+  translate(code){
+    if(this.members.has(code)){
+      return this.members.get(code);
+    }
+    else{
+      console.log('Unknown error');
+    }
+      
+  }
+
+}
